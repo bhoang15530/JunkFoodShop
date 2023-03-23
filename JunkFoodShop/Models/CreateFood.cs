da2dae168bdb18c2b3c739ac15191dbc3d54bb17
bhoang15530 <bhoang15530@gmail.com>
2023-03-23 16:13:13 +0700

    UPDATE ORDER STATUS SET

--- JunkFoodShop/Models/CreateFood.cs
+++ JunkFoodShop/Models/CreateFood.cs
@@ -4,22 +4,16 @@ namespace JunkFoodShop.Models
 {
     public class CreateFood
     {
-        [Required]
         public string FoodName { get; set; }
 
-        [Required]
         public string FoodImage { get; set; }
 
-        [Required]
         public int FoodPrice { get; set; }
 
-        [Required]
         public int FoodStock { get; set; }
 
-        [Required]
         public string FoodDescription { get; set; }
 
-        [Required]
         public int CategoryId { get; set; }
     }
 }
